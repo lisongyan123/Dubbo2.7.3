7327ae55bc18a802875dedb918a3eea069f0b173
842166100@qq.com <lisongyan1996>
2020-06-14 00:05:07 +0800

    注解

---
+++ dubbo-public-utils/src/main/java/com/lsy/dubbo/utils/javaBase/annotation/Annotation.java
@@ -0,0 +1,12 @@
+//package com.lsy.dubbo.utils.javaBase.annotation;
+//
+//public interface Annotation {
+//
+//    boolean equals(Object obj);
+//
+//    int hashCode();
+//
+//    String toString();
+//
+//    Class<? extends Annotation> annotationType();
+//}
